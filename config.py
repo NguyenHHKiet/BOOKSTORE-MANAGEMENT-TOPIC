452347ff3ce51b113807477604d207f22188efa4
huydangapi2707 <dangdinhhuyisme@gmail.com>
2023-11-22 15:45:34 +0700

    update models

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Create dummy secrey key so we can use sessions
+from urllib.parse import quote
+
 SECRET_KEY = 'c007d6402c1b77b1fac427a381d995fd'
 
 # Debug mode
@@ -6,8 +8,8 @@ DEBUG = True
 
 # Create in-memory database
 DATABASE_FILE = 'bookstore'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/'+ DATABASE_FILE +'?charset=utf8mb4' # Non_password_MySQL
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:%s@localhost/bookstore?charset=utf8mb4' % quote('password_of_MySQLDatabase')
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/'+ DATABASE_FILE +'?charset=utf8mb4' # Non_password_MySQL
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:%s@localhost/bookstore?charset=utf8mb4' % quote('admin')
 
 # Flask-Security config
 SECURITY_URL_PREFIX = "/admin"
